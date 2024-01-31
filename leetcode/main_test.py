e0115d2774badb6b1b504fb9343582dd38472f96
SleepyLink1 <88170820+SleepyLink1@users.noreply.github.com>
2024-01-31 11:14:23 -0600

    added main code and test

--- leetcode/main_test.py
+++ leetcode/main_test.py
@@ -1,6 +1,7 @@
 import unittest
 from main import is_palindrome, fibonacci
 
+
 class TestLeetCode(unittest.TestCase):
     def test_is_palindrome(self):
         self.assertTrue(is_palindrome("racecar"))
@@ -11,6 +12,26 @@ class TestLeetCode(unittest.TestCase):
         self.assertEqual(fibonacci(1), 1)
         self.assertEqual(fibonacci(5), 5)
         self.assertEqual(fibonacci(10), 55)
+
+    def test_is_palindrome_with_numbers(self):
+        self.assertTrue(is_palindrome("12321"))
+        self.assertFalse(is_palindrome("12345"))
+
+    def test_is_palindrome_with_special_characters(self):
+        self.assertTrue(is_palindrome("!@#$%$#@!"))
+        self.assertFalse(is_palindrome("!@#$%^"))
+
+    def test_is_palindrome_with_empty_string(self):
+        self.assertTrue(is_palindrome(""))
+
+    # Additional test cases for fibonacci
+    def test_fibonacci_with_negative_numbers(self):
+        with self.assertRaises(ValueError):
+            fibonacci(-1)
+
+    def test_fibonacci_with_large_numbers(self):
+        self.assertEqual(fibonacci(20), 6765)
+        self.assertEqual(fibonacci(30), 832040)
 
 if __name__ == '__main__':
     unittest.main()
